f7137d61cc2ca06f53a694bd50368b08d56ed259
Abner0627 <63701415+Abner0627@users.noreply.github.com>
2021-06-16 22:15:05 +0800

    6/16

--- abner/01.py
+++ abner/01.py
@@ -2,6 +2,22 @@ import os
 from os import listdir
 import shutil
 
+#%% Func
+def _split(path):
+    JP = []
+    L = listdir(path)
+    datatype = path.split('./')[-1]
+    for i in range(len(L)):
+        f = L[i]
+        ff = f.split('.')
+        if ff[-1]=='jpg':
+            JP.append('./'+ datatype + '/' + f)
+    with open('./waymo_object_detection/cfg/' + datatype + '.txt', 'w') as output:
+        for row in JP:
+            output.write(str(row) + '\n')            
+    return JP
+
+
 #%% 建立資料夾
 if not os.path.exists('waymo_object_detection'):
     os.mkdir('waymo_object_detection')
@@ -20,5 +36,29 @@ if not os.path.exists('waymo_object_detection/cfg/face.names'):
 tra = './train'
 val = './valid'
 
-tra_list = listdir(tra)
-val_list = listdir(val)
+tra_list = _split(tra)
+val_list = _split(val)
+
+
+#%% NAMES
+classes = [
+    'TYPE_UNKNOWN',
+    'TYPE_VEHICLE',
+    'TYPE_PEDESTRIAN',
+    'TYPE_SIGN',
+    'TYPE_CYCLIST'
+]
+
+with open('./waymo_object_detection/cfg/object.names',mode='w') as f:   
+    for c in classes:
+        f.write(c)
+        f.write('\n')
+
+#%% DATA
+with open('./waymo_object_detection/cfg/object.data',mode='w') as f:   
+    f.write(f'classes = {len(classes)}\n')
+    f.write(f'train = ./waymo_object_detection/cfg/train.txt\n')
+    f.write(f'valid = ./waymo_object_detection/cfg/val.txt\n')
+    f.write(f'names = ./waymo_object_detection/cfg/object.names\n')
+    f.write(f'backup = ./waymo_object_detection/cfg/weights\n')
+
