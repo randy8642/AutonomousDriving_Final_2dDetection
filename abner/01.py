69f83f526f6911ec5b7aa66f37308b35df58d580
Abner0627 <63701415+Abner0627@users.noreply.github.com>
2021-06-17 14:51:31 +0800

    6/17

--- abner/01.py
+++ abner/01.py
@@ -17,6 +17,36 @@ def _split(path):
             output.write(str(row) + '\n')            
     return JP
 
+def _split2(dic):
+    TRA = dic['train']
+    VAL = dic['valid']   
+    # train
+    TRAL = []
+    for idx_tra in TRA:
+        L = listdir(idx_tra)
+        for i in range(len(L)):
+            f = L[i]
+            ff = f.split('.')
+            if ff[-1]=='jpg':
+                TRAL.append('.'+ idx_tra + '/' + f) 
+    with open('./waymo_object_detection/cfg/train.txt', 'w') as output:
+        for row in TRAL:
+            output.write(str(row) + '\n')        
+
+    # valid
+    VALL = []
+    for idx_val in VAL:
+        L = listdir(idx_val)
+        for i in range(len(L)):
+            f = L[i]
+            ff = f.split('.')
+            if ff[-1]=='jpg':
+                VALL.append('.'+ idx_val + '/' + f) 
+    with open('./waymo_object_detection/cfg/valid.txt', 'w') as output:
+        for row in VALL:
+            output.write(str(row) + '\n') 
+    return TRAL, VALL          
+
 
 #%% 建立資料夾
 if not os.path.exists('waymo_object_detection'):
@@ -33,12 +63,17 @@ if not os.path.exists('waymo_object_detection/cfg/face.names'):
     shutil.copyfile('darknet/cfg/coco.names', 'waymo_object_detection/cfg/object.names')
 
 #%% DataList
-tra = './train'
-val = './valid'
+data_dict = {
+    'train':['./train_0', './train_1'],
+    'valid':['./valid_0']
+}
+# tra = './train'
+# val = './valid'
 
-tra_list = _split(tra)
-val_list = _split(val)
+# tra_list_0 = _split(tra)
+# val_list_0 = _split(val)
 
+tra, val = _split2(data_dict)
 
 #%% NAMES
 classes = [
@@ -58,7 +93,7 @@ with open('./waymo_object_detection/cfg/object.names',mode='w') as f:
 with open('./waymo_object_detection/cfg/object.data',mode='w') as f:   
     f.write(f'classes = {len(classes)}\n')
     f.write(f'train = ../waymo_object_detection/cfg/train.txt\n')
-    f.write(f'valid = ../waymo_object_detection/cfg/val.txt\n')
+    f.write(f'valid = ../waymo_object_detection/cfg/valid.txt\n')
     f.write(f'names = ../waymo_object_detection/cfg/object.names\n')
-    f.write(f'backup = ../waymo_object_detection/cfg/weights\n')
+    f.write(f'backup = backup\n')
 
