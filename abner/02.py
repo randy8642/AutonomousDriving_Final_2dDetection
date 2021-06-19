e1b0b00dcd0b7c1b8b2341f694abf25153ecbfe5
Abner0627 <n16090065@gs.ncku.edu.tw>
2021-06-19 14:55:43 +0800

    Update 02.py

--- abner/02.py
+++ abner/02.py
@@ -5,13 +5,19 @@ from os import listdir
 
 def _img(L):
     nL = []
+    nsL = []
     for i in L:
         t = i.split('.')[-1]
         if t == 'jpg':
             nL.append(i)
-    return nL
+    for j in range(len(nL)):
+        f = str(j) + '.jpg'
+        nsL.append(f)
+    return nsL
 
-path = './valid_0'
+
+
+path = './valid_00'
 L = listdir(path)
 nL = _img(L)
-nsL = sorted(nL)
+
